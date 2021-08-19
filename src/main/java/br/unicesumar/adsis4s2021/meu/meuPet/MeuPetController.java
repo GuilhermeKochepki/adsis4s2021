e457fed0e54131592df6baf954b1ae1168aeff23
GuilhermeKochepki <gui.kochepki@gmail.com>
2021-08-19 08:32:13 -0300

    Aplicando crud com persistencia com JPA

--- src/main/java/br/unicesumar/adsis4s2021/meu/meuPet/MeuPetController.java
+++ src/main/java/br/unicesumar/adsis4s2021/meu/meuPet/MeuPetController.java
@@ -1,4 +1,4 @@
-package br.unicesumar.adsis4s2021.meu.meuProduto;
+package br.unicesumar.adsis4s2021.meu.meuPet;
 
 import java.util.List;
 
@@ -12,11 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/meu-produto")
-public class MeuProdutoController {
+@RequestMapping("/meu-pet")
+public class MeuPetController {
 	//conectar no banco
 	@Autowired
-	private MeuProdutoRepository repo;
+	private MeuPetRepository repo;
 	
 	@DeleteMapping("/{id}")
 	public void deletePeloId(@PathVariable ("id") String id) {
@@ -25,19 +25,19 @@ public class MeuProdutoController {
 	
 	//retornar
 	@GetMapping
-	public List<MeuProduto> getProdutos() {
+	public List<MeuPet> getProdutos() {
 		return repo.findAll();
 	}
 	
 	//incluir
 	@PostMapping
-	public String postProduto(@RequestBody MeuProduto novo) {
+	public String postProduto(@RequestBody MeuPet novo) {
 		novo = repo.save(novo);
 		return novo.getId();
 	}
 	
 	@GetMapping("/{id}")
-	public MeuProduto getProdutoPeloId(@PathVariable ("id") String id) {
+	public MeuPet getProdutoPeloId(@PathVariable ("id") String id) {
 		return repo.findById(id).get();
 	}
 
