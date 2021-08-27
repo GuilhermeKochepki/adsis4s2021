0deff5ab45bcd25cf96472659db3b19fb7a84271
GuilhermeKochepki <gui.kochepki@gmail.com>
2021-08-27 20:37:25 -0300

    Inserindo novo projeto de Pedido

---
+++ src/main/java/br/unicesumar/adsis4s2021/meu/meuPedido/MeuPedidoController.java
@@ -0,0 +1,31 @@
+package br.unicesumar.adsis4s2021.meu.meuPedido;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/meus-pedidos")
+public class MeuPedidoController {
+	@Autowired
+	private MeuPedidoRepository repo;
+	
+	@GetMapping
+	public List<MeuPedido> getAll() {
+		return repo.findAll();
+	}
+	
+	@PostMapping
+	public String post(@RequestBody MeuPedido novo) {
+		if (repo.findById(novo.getId()).isPresent()) {
+			throw new RuntimeException("Seu pedido já existe, faça um put ao invés de post!");
+		}
+		novo = repo.save(novo);
+		return novo.getId();
+	}
+}
