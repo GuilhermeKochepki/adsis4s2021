47324318c85402f58e3cad9f8e4fa54f32a72d60
GuilhermeKochepki <gui.kochepki@gmail.com>
2021-08-27 19:46:40 -0300

    Ampliando a estrutura, e funções do Pet, assim como foi feito em produto

--- src/main/java/br/unicesumar/adsis4s2021/meu/meuPet/MeuPetController.java
+++ src/main/java/br/unicesumar/adsis4s2021/meu/meuPet/MeuPetController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,31 +15,42 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/meus-pets")
 public class MeuPetController {
-	//conectar no banco
 	@Autowired
 	private MeuPetRepository repo;
 	
 	@DeleteMapping("/{id}")
-	public void deletePeloId(@PathVariable ("id") String id) {
+	public void deletePeloId(@PathVariable("id") String id) {
 		repo.deleteById(id);
 	}
 	
-	//retornar
+	@GetMapping("/{id}")
+	public MeuPet getPeloId(@PathVariable("id") String id) {
+		return repo.findById(id).get();
+	}
+	
 	@GetMapping
-	public List<MeuPet> getPet() {
+	public List<MeuPet> getAll() {
 		return repo.findAll();
 	}
 	
-	//incluir
 	@PostMapping
-	public String postPet(@RequestBody MeuPet novo) {
+	public String post(@RequestBody MeuPet novo) {
+		if (repo.findById(novo.getId()).isPresent()) {
+			throw new RuntimeException("Seu Pet já existe, faça um put ao invés de post!");
+		}
 		novo = repo.save(novo);
 		return novo.getId();
 	}
 	
-	@GetMapping("/{id}")
-	public MeuPet getPetPeloId(@PathVariable ("id") String id) {
-		return repo.findById(id).get();
-	}
-
+	@PutMapping("/{id}")
+	public String put(@RequestBody MeuPet modificado, @PathVariable("id") String id) {
+		if (!modificado.getId().equals(id)) {
+			throw new RuntimeException("Para atualizar um Pet, os IDs do request devem ser iguais!");
+		}
+		if (!repo.findById(id).isPresent()) {
+			throw new RuntimeException("Seu Pet não existe, faça um post ao invés de put!");
+		}
+		modificado = repo.save(modificado);
+		return modificado.getId();
+	}	
 }
