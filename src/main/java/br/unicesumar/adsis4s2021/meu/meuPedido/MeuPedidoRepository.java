41f14439ffc69bc2fa04ab7c07e745c2fb793546
GuilhermeKochepki <gui.kochepki@gmail.com>
2021-10-20 15:40:16 -0300

    Enfim resolvido problemas relacionados a DTO.
    Ocasionado pelo esquecimento de trocar dentro da Query, pedido por
    MeuPedido ou produto por MeuProduto

--- src/main/java/br/unicesumar/adsis4s2021/meu/meuPedido/MeuPedidoRepository.java
+++ src/main/java/br/unicesumar/adsis4s2021/meu/meuPedido/MeuPedidoRepository.java
@@ -6,32 +6,36 @@ import java.util.Map;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import br.unicesumar.adsis4s2021.meu.meuPedido.dto.MeuItemPedidoDTO;
+import br.unicesumar.adsis4s2021.meu.meuPedido.dto.MeuPedidoDTO;
+import br.unicesumar.adsis4s2021.meu.meuPedido.dto.MeuTotalVendidoDoProdutoDTO;
+
 public interface MeuPedidoRepository extends JpaRepository<MeuPedido, String>{
 	
-	//@Query(value = "select p.nome as produto, "
-	//		+ "sum(ip.quantidade = ip.preco_unitario) as \"totalVendido\" "
-	//		+ "from item_pedido ip "
-	//		+ "inner join produto p on p.id = ip.produto_id "
-	//		+ "group by p.nome ", nativeQuery = true)
-	//List<Map<String, Object>> consultarTotalVendidoPorProduto();
-	
-	@Query(value = "select p.nome as produto, "
+	@Query(value = "select p.nome as MeuProduto, "
 			+ "sum(ip.quantidade * ip.precoUnitario) as totalVendido "
 			+ "from MeuItemPedido ip "
-			+ "inner join ip.produto p "
+			+ "inner join ip.MeuProduto p "
 			+ "group by p.nome")
 	List<Map<String, Object>> consultarTotalVendidoPorProduto();
 	
-	//@Query(value = "select new br.unicesumar.adsis4s2021.meu.meuPedido.meuDto.MeuTotalVendidoDoProdutoDTO(p.nome, "
-	//		+ "sum(ip.quantidade * ip.precoUnitario)) "
-	//		+ "from MeuItemPedido ip "
-	//		+ "inner join ip.produto p "
-	//		+ "group by p.nome")
-	//List<MeuTotalVendidoDoProdutoDTO> consultarTotalVendidoPorProdutoDTO();
+	@Query(value = "select new br.unicesumar.adsis4s2021.meu.meuPedido.meuDto.MeuTotalVendidoDoProdutoDTO(p.nome, "
+			+ "sum(ip.quantidade * ip.precoUnitario)) "
+			+ "from MeuItemPedido ip "
+			+ "inner join ip.MeuProduto p "
+			+ "group by p.nome")
+	List<MeuTotalVendidoDoProdutoDTO> consultarTotalVendidoPorProdutoDTO();
 	
-	//@Query(value = "select new br.unicesumar.adsis4s2021.meu.meuPedido.meuDto.MeuPedidoDTO"
-	//		+ "(p.id, p.numero, p.emitidoEm, c.id, c.nome) "
-	//		+ "from MeuPedido "
-	//		+ "inner join p.cliente c")
-	//List<MeuPedidoDTO> meuEncontrarTodosComoDTO();
+	@Query(value = "select new br.unicesumar.adsis4s2021.meu.meuPedido.meuDto.MeuPedidoDTO"
+			+ "(p.id, p.numero, p.emitidoEm, c.id, c.nome) "
+			+ "from MeuPedido "
+			+ "inner join p.cliente c")
+	List<MeuPedidoDTO> meuEncontrarTodosComoDTO();
+	
+	@Query(value = "select new br.unicesumar.adsis4s2021.meu.meuPedido.meuDto.MeuItemPedidoDTO(ip.id, ip.quantidade, ip.percentualDeDesconto, ip.precoUnitario, prod.id, prod.nome) "
+			+ "from MeuPedido p "
+			+ "inner join p.itens ip "
+			+ "inner join ip.MeuProduto prod "
+			+ "where p.id = :pedidoId")
+	List<MeuItemPedidoDTO> encontrarItensPedidoDoPedidoDTO(String pedidoId);
 }
